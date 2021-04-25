8077f7b2c38f52b1d5e885b15f48fdd6f0f94f47
Mason Elmenhurst <elmenhm2@cf420-00.cs.wwu.edu>
2021-04-24 23:03:10 -0700

    next update

--- Player.java
+++ Player.java
@@ -20,47 +20,77 @@ public class Player {
     private double lootModifier;
 
     public Player(String pClass){
-
         playerClass = pClass;
 
         if (playerClass == warriorClass){
             //TODO: change hp if needed
             setHealth(100);
             setDamage(15);
+            lootModifier = 1.00;
         }
         else{
             setHealth(70);
             setDamage(10);
+            lootModifier = 1.20;
         }
 
         setGold(0);
 
+
     }
 
+    //TODO: attack monster
     /* Hits the targeted Monster */
     public void attack(Monster target) {
+
+        target.onHit(damage);
 
     }
 
 	/* Removes health from this Player
     when hit by a Monster */
     public void onHit(int damage) {
-
-        if (){
-            
+        //TODO: move this to sethealth
+        if ((health - damage) < 0){
+            health = 0;
         }
-        health -= damage;
+        else{
+            health -= damage;
+        }
 
     }
 
 	/* Adds health to this Player when healed */
-    public void onHeal(int health) {
+    public void onHeal(int heal) {
+        //TODO: move this to sethealth
+        if(playerClass == warriorClass && health == 100){
+            health = 100;
+        }
+        else if (playerClass == warriorClass && ((health + heal) > 100)){
+            health = 100;
+        }
+        else if (playerClass == thiefClass && health == 70){
+            health = 70;
+        }
+        else if (playerClass == thiefClass && ((health + heal) > 70)){
+            health = 70;
+        }
+        else{
+            health += heal;
+        }
 
     }
 
 	/* Adds gold to this Player when obtained */
-    public void onLoot(int gold) {
-
+    public void onLoot(int goldAdded) {
+        //TODO: move this to setgold
+        if (playerClass == warriorClass){
+            gold += goldAdded;
+        } 
+        else{
+            goldAdded += goldAdded * 0.2;
+            gold += goldAdded;
+        }
     }
 
     public int getHealth() {
@@ -68,7 +98,7 @@ public class Player {
     }
 
     public void setHealth(int health) {
-        this.health = health;
+        this.health += health;
     }
 
     public int getGold() {
@@ -76,7 +106,7 @@ public class Player {
     }
 
     public void setGold(int gold) {
-        this.gold = gold;
+        this.gold += gold;
     }
 
     public int getDamage() {
@@ -84,6 +114,6 @@ public class Player {
     }
 
     public void setDamage(int damage) {
-        this.damage = damage;
+        this.damage += damage;
     }
 }
