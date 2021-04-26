c0d13ec58246aacb25a76b492edc315d4aa1b4c6
joshramos1 <82556237+joshramos1@users.noreply.github.com>
2021-04-25 21:01:49 -0700

    Update DungeonGame.java

--- DungeonGame.java
+++ DungeonGame.java
@@ -59,15 +59,30 @@ public class DungeonGame {
             
          
         if (chooseClass == 2){
-            new Player("thief");
             Player player = new Player("thief");
             DungeonMap map = new DungeonMap(10, 20, player);
-            map.print();
-            break;
+            while (true){
+                System.out.println("==========================");
+                map.print();
+                String move = input.next();
+                if (move.equals("w")){
+                    map.move(player, -1, 0);
+                }
+                else if (move.equals("a")){
+                    map.move(player, 0, -1);
+                }
+                else if (move.equals("s")){
+                    map.move(player, 1, 0); 
+                }
+                else if (move.equals("d")){
+                    map.move(player, 0, 1);
+                }
+                }
         }
         else{
             System.out.println("Please input either [1] Warrior or [2] Theif");
         }
     }
 }
-}  
+}    
+
